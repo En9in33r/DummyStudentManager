afdfd25759034ecc3ec8b0e2e536724672d01086
En9in33r <semenvad1@gmail.com>
2019-02-18 22:10:13 +0800

    + subject name in ScheduleAdapter

--- app/src/main/java/com/example/dummystudentmanager/schedule/ScheduleAdapter.java
+++ app/src/main/java/com/example/dummystudentmanager/schedule/ScheduleAdapter.java
@@ -1,16 +1,21 @@
 package com.example.dummystudentmanager.schedule;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.example.dummystudentmanager.R;
 import com.example.dummystudentmanager.discipline.DisciplineItem;
+import com.example.dummystudentmanager.discipline.DisciplinesDB;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ScheduleAdapter extends BaseAdapter {
     Context context;
@@ -53,12 +58,30 @@ public class ScheduleAdapter extends BaseAdapter {
 
         final ScheduleItem d = getScheduleItem(position);
 
+        // необходимо вытащить из базы данных Предметы элемент, у которого ID равно полученному disc_id, и вытащить имя
+
+        DisciplinesDB helper_inside = new DisciplinesDB(context); // нужно вытащить из базы данных предметов (дисциплин) все строки, поместить имена в этот спиннер
+        SQLiteDatabase database_inside = helper_inside.getReadableDatabase();
+        Cursor cursor = database_inside.query(DisciplinesDB.TABLE_FAVOURITES,
+                null,
+                "_id = ?",
+                new String[] { Integer.toString(d.disc_id + 1) },
+                null, null, null);
 
 
-        if (d.disc_id == 0)
+        // magic code from StackOverflow
+
+        cursor.moveToFirst();
+        if (!cursor.isAfterLast())
         {
-            ((TextView)view.findViewById(R.id.disciplineNameTextView)).setText("жопа");
+            //subjectArray.add(cursor.getString(cursor
+            //        .getColumnIndex(DisciplinesDB.KEY_NAME)));
+            //cursor.moveToNext();
+
+            ((TextView)view.findViewById(R.id.disciplineNameTextView)).setText(cursor.getString(cursor.getColumnIndex(DisciplinesDB.KEY_NAME)));
         }
+        cursor.close();
+
         /*((TextView)view.findViewById(R.id.disciplineNameTextView)).setText(d.disc_id);
         ((TextView)view.findViewById(R.id.dayTextView)).setText(d.day);
         ((TextView)view.findViewById(R.id.timeTextView)).setText(d.time);
